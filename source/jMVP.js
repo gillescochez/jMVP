ddfa9cf0e7647553445b6dc1879fa2b4c6973d75
gillescochez <git@csurl.eu>
2013-05-01 12:12:41 +0100

    Added comment (nearly forgot to put some!)

--- source/jMVP.js
+++ source/jMVP.js
@@ -1,11 +1,25 @@
+/**
+ * Create a new jMVP instance
+ * @param model
+ * @param view
+ * @param presenter
+ */
 function jMVP(model, view, presenter) {
 
 };
 
+/**
+ * Create a new jMVP View instance
+ * @constructor
+ */
 jMVP.View = function() {
 
 };
 
+/**
+ * Create a new jMVP Presenter instance
+ * @constructor
+ */
 jMVP.Presenter = function() {
 
 };
