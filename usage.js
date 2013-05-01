c029763ea1aa05ca95110bdcc7be9350f2b4fcad
gillescochez <git@csurl.eu>
2013-05-01 12:08:05 +0100

    Added some more usage tryouts

--- usage.js
+++ usage.js
@@ -5,10 +5,24 @@ var model = {
 var view = new jMVP.View();
 view.template('<div>{{$foo}}</div>');
 
-var presenter = new jMVP.Presenter();
+var presenter = new jMVP.Presenter({
+	foo: {
+		format: {
+			// those can be array to handle multiple format converters
+			onSetValue: function() {},
+			onGetValue: function() {}
+		}
+	}
+});
 
-var mvp = new jMVP(model, view, presenter);
+var jmvp = new jMVP(model, view, presenter);
 
-mvp.model.foo.setValue('TEST');
-mvp.view.template('<span>{{$foo}}</span>');
+jmvp.model.foo.setValue('TEST');
+jmvp.view.template('<span>{{$foo}}</span>');
+
+
+// load files and return a jMVP instance ready to use
+jMVP.import('model', 'view', 'presenter', function(jmvp) {
+
+});
 
