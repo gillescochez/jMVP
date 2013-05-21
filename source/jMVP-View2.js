67adb0e15157797403a12b299adcd7302b9552ec
gillescochez <git@csurl.eu>
2013-05-21 22:22:24 +0100

    Getting thought ready for View rewrite

---
+++ source/jMVP-View2.js
@@ -0,0 +1,25 @@
+jMVP.View = function() {
+
+};
+
+/*
+    Stuff it needs
+
+        Map - same as current
+
+        Global config object parsing method
+            - Loop parser method
+                -> Map generated
+
+        Recursive DOM generation method
+            - Only handle DOM creation nothing else
+
+        Fast access / return of DOM element
+            - Using the current oNodesMap
+
+        Apply hooks
+            - applyToRef(sReference)
+            - applyAll()
+
+        
+*/
